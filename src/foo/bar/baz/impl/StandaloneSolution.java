286186095e89642873e86201d73ebc2a915cf67f
dasha.garanina <dasha.garanina@gmail.com>
2018-08-08 01:11:39 +0300

    task1

--- src/foo/bar/baz/impl/StandaloneSolution.java
+++ src/foo/bar/baz/impl/StandaloneSolution.java
@@ -12,6 +12,7 @@ import java.util.concurrent.BlockingQueue;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.stream.Stream;
 
 public class StandaloneSolution implements Solution {
 
@@ -20,13 +21,9 @@ public class StandaloneSolution implements Solution {
 
     @Override
     public CompletableFuture<BigDecimal> leibnizPi(Integer accuracy) {
-
         printCurrentThread();
-
         producer(accuracy);
-
         CompletableFuture<BigDecimal> future = consumer(accuracy);
-
         shutdown();
         return future;
     }
@@ -43,6 +40,7 @@ public class StandaloneSolution implements Solution {
     /**
      * Starts Producer in executor service thread pool
      * Async
+     *
      * @param accuracy - the number of decimal places in the calculated Pi
      */
     private void producer(Integer accuracy) {
@@ -54,23 +52,20 @@ public class StandaloneSolution implements Solution {
      * Starts Consumers in executor service thread pool
      * Using by {@link #leibnizPi(Integer)}
      * Async
+     *
      * @param accuracy - the number of decimal places in the calculated Pi
      * @return Pi
      */
     private CompletableFuture<BigDecimal> consumer(Integer accuracy) {
-        int lim = (int) (Math.pow(10, accuracy) * 2 + 12);
-
-        BigDecimal temp1 = BigDecimal.valueOf(2).multiply(new BigDecimal(lim)).subtract(BigDecimal.ONE);
-        BigDecimal last = BigDecimal.valueOf(4).divide(temp1, 100, RoundingMode.HALF_UP);
-
-        CompletableFuture<BigDecimal> future1 = CompletableFuture.supplyAsync(new Consumer(blockingQueue)::call, executor);
-        CompletableFuture<BigDecimal> future2 = CompletableFuture.supplyAsync(new Consumer(blockingQueue)::call, executor);
-        CompletableFuture<BigDecimal> future3 = CompletableFuture.supplyAsync(new Consumer(blockingQueue)::call, executor);
-
-        return future1
-                .thenCombineAsync(future2, BigDecimal::add)
-                .thenCombineAsync(future3, BigDecimal::add)
+        return Stream.of(1, 2, 3, 4)
+                .map(x -> CompletableFuture.supplyAsync(new Consumer(blockingQueue)::call, executor))
+                .reduce(CompletableFuture.completedFuture(BigDecimal.ZERO), (f1, f2) -> f1.thenCombineAsync(f2, BigDecimal::add))
                 .thenApplyAsync(c -> {
+                    System.out.println("Callback: " + Thread.currentThread().getName());
+                    BigDecimal temp1 = BigDecimal.valueOf(2)
+                            .multiply(BigDecimal.valueOf((Math.pow(10, accuracy) * 2 + 12)))
+                            .subtract(BigDecimal.ONE);
+                    BigDecimal last = BigDecimal.valueOf(4).divide(temp1, 100, RoundingMode.DOWN);
                     BigDecimal result = c.add(c.add(last));
                     return result.divide(BigDecimal.valueOf(2), 100, RoundingMode.DOWN);
                 });
@@ -80,32 +75,14 @@ public class StandaloneSolution implements Solution {
      * Starts Consumers in executor service thread pool
      * Using by {@link #leibnizPi2(Integer)}
      * Async
+     *
      * @return Pi
      */
     private CompletableFuture<BigDecimal> consumer2() {
-        CompletableFuture<BigDecimal> future1 = CompletableFuture.supplyAsync(new Consumer2(blockingQueue)::call, executor);
-        CompletableFuture<BigDecimal> future2 = CompletableFuture.supplyAsync(new Consumer2(blockingQueue)::call, executor);
-        CompletableFuture<BigDecimal> future3 = CompletableFuture.supplyAsync(new Consumer2(blockingQueue)::call, executor);
-        CompletableFuture<BigDecimal> future4 = CompletableFuture.supplyAsync(new Consumer2(blockingQueue)::call, executor);
-        CompletableFuture<BigDecimal> future5 = CompletableFuture.supplyAsync(new Consumer2(blockingQueue)::call, executor);
-        CompletableFuture<BigDecimal> future6 = CompletableFuture.supplyAsync(new Consumer2(blockingQueue)::call, executor);
-        CompletableFuture<BigDecimal> future7 = CompletableFuture.supplyAsync(new Consumer2(blockingQueue)::call, executor);
-        CompletableFuture<BigDecimal> future8 = CompletableFuture.supplyAsync(new Consumer2(blockingQueue)::call, executor);
-        CompletableFuture<BigDecimal> future9 = CompletableFuture.supplyAsync(new Consumer2(blockingQueue)::call, executor);
-        CompletableFuture<BigDecimal> future10 = CompletableFuture.supplyAsync(new Consumer2(blockingQueue)::call, executor);
-
-        CompletableFuture<BigDecimal> future = future1
-                .thenCombineAsync(future2, BigDecimal::add)
-                .thenCombineAsync(future3, BigDecimal::add)
-                .thenCombineAsync(future4, BigDecimal::add)
-                .thenCombineAsync(future5, BigDecimal::add)
-                .thenCombineAsync(future6, BigDecimal::add)
-                .thenCombineAsync(future7, BigDecimal::add)
-                .thenCombineAsync(future8, BigDecimal::add)
-                .thenCombineAsync(future9, BigDecimal::add)
-                .thenCombineAsync(future10, BigDecimal::add);
-
-        return future.thenApplyAsync(bigDecimal -> bigDecimal.multiply(BigDecimal.valueOf(Math.sqrt(12))));
+        return Stream.of(1, 2, 3, 4)
+                .map(x -> CompletableFuture.supplyAsync(new Consumer2(blockingQueue)::call, executor))
+                .reduce(CompletableFuture.completedFuture(BigDecimal.ZERO), (f1, f2) -> f1.thenCombineAsync(f2, BigDecimal::add))
+                .thenApplyAsync(bigDecimal -> bigDecimal.multiply(BigDecimal.valueOf(Math.sqrt(12))));
     }
 
     /**
