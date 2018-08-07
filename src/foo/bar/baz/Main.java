8affff1e771b439df4a53108b435f0b0a16ff192
dariag <dasha.garanina@gmail.com>
2018-08-07 13:39:51 +0300

    temp

--- src/foo/bar/baz/Main.java
+++ src/foo/bar/baz/Main.java
@@ -3,22 +3,23 @@ package foo.bar.baz;
 import foo.bar.baz.impl.StandaloneSolution;
 
 import java.math.BigDecimal;
-import java.util.concurrent.ArrayBlockingQueue;
-import java.util.concurrent.BlockingQueue;
+import java.math.RoundingMode;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ExecutionException;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-import java.util.concurrent.Future;
 
 public class Main {
 
 	public static void main(String[] args) {
 		Solution solution = new StandaloneSolution();
 		try {
-			CompletableFuture<BigDecimal> future = solution.leidnizPi(5);
-			BigDecimal result = future.get();
-			System.out.println(result);
+			long start = System.currentTimeMillis();
+
+			Integer accuracy = 5;
+			CompletableFuture<BigDecimal> future = solution.leidnizPi(accuracy);
+			BigDecimal result = future.get().setScale(accuracy, RoundingMode.DOWN);
+			System.out.println("Result: " + result);
+			long end = System.currentTimeMillis();
+			System.out.println("Time spend: " + (end-start));
 		} catch (InterruptedException | ExecutionException e) {
 			e.printStackTrace();
 		}
