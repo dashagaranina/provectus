1f923e506b78d0037812f02943a37d9fe83e0396
dasha.garanina <dasha.garanina@gmail.com>
2018-08-11 17:05:46 +0300

    eureka-service

--- master-service/src/main/java/com/provectus/task2/Task2Application.java
+++ master-service/src/main/java/com/provectus/task2/Task2Application.java
@@ -2,15 +2,16 @@ package com.provectus.task2;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 import org.springframework.web.client.RestTemplate;
 
 import java.util.concurrent.Executor;
-
+@EnableDiscoveryClient
 @SpringBootApplication
-@EnableAsync
+//@EnableAsync
 public class Task2Application {
 
     public static void main(String[] args) {
--- master-service/src/main/java/com/provectus/task2/service/impl/DistributedSolution.java
+++ master-service/src/main/java/com/provectus/task2/service/impl/DistributedSolution.java
@@ -3,8 +3,11 @@ package com.provectus.task2.service.impl;
 import com.provectus.task2.service.Solution;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpMethod;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.util.UriComponentsBuilder;
 
 import java.math.BigDecimal;
 import java.math.RoundingMode;
@@ -17,36 +20,31 @@ public class DistributedSolution implements Solution {
 
     private final RestTemplate restTemplate;
 
+    private final static String URL = "http://localhost:8081/pi";
+
     @Override
     public BigDecimal leibniz(Integer accuracy) {
         long lim = (long) (Math.pow(10, accuracy) * 2 + 12);
         int n = (int) (lim / 3);
-  /*      Map<String, Integer> params = new HashMap<>();
-        params.put("left", 1);
-        params.put("right", n);*/
+        int l = 1;
         int r = n;
-        log.info("Cons2, left = 1, right = {}", r);
-        BigDecimal result1 = restTemplate.getForObject("http://localhost:8081/pi?left=1&right="+r, BigDecimal.class);
-//        BigDecimal result1 = restTemplate.getForObject("http://localhost:8081/pi", BigDecimal.class, params);
+        String url = getUriComponentsBuilder(l, r);
 
-/*        params = new HashMap<>();
-        params.put("left", n + 1);
-        params.put("right", 2 * n);*/
-        r = 2*n;
-        int l = n+1;
-        log.info("Cons3, left = {}, right = {}", l, r);
-        BigDecimal result2 = restTemplate.getForObject("http://localhost:8081/pi?left="+l+"&right="+r, BigDecimal.class);
-//        BigDecimal result2 = restTemplate.getForObject("http://localhost:8081/pi", BigDecimal.class, params);
+        log.info("Cons1, left = {}, right = {}, URL = {}", l, r, url);
+        BigDecimal result1 = restTemplate.getForObject(url,
+                BigDecimal.class);
 
+        r = 2 * n;
+        l = n + 1;
+        url = getUriComponentsBuilder(l, r);
+        log.info("Cons2, left = {}, right = {}, URL = {}", l, r, url);
+        BigDecimal result2 = restTemplate.getForObject(url, BigDecimal.class);
 
-/*        params = new HashMap<>();
-        params.put("left", 2 * n + 1);
-        params.put("right", (int) lim);*/
         r = (int) lim;
-        l = (2*n+1);
-        log.info("Cons1, left = {}, right = {}", l, r);
-        BigDecimal result3 = restTemplate.getForObject("http://localhost:8081/pi?left="+l+"&right="+r, BigDecimal.class);
-//        BigDecimal result3 = restTemplate.getForObject("http://localhost:8081/pi", BigDecimal.class, params);
+        l = (2 * n + 1);
+        url = getUriComponentsBuilder(l, r);
+        log.info("Cons3, left = {}, right = {}, URL = {}", l, r, url);
+        BigDecimal result3 = restTemplate.getForObject(url, BigDecimal.class);
 
         BigDecimal c = result1.add(result2).add(result3);
         BigDecimal temp1 = BigDecimal.valueOf(2)
@@ -54,7 +52,15 @@ public class DistributedSolution implements Solution {
                 .subtract(BigDecimal.ONE);
         BigDecimal last = BigDecimal.valueOf(4).divide(temp1, 100, RoundingMode.DOWN);
         BigDecimal result = c.add(c.add(last));
+
         return result.divide(BigDecimal.valueOf(2), 100, RoundingMode.DOWN).setScale(accuracy, RoundingMode.DOWN);
+    }
+
+    private String getUriComponentsBuilder(int left, int right) {
+        UriComponentsBuilder uriComponentsBuilder = UriComponentsBuilder.fromHttpUrl(URL)
+                .queryParam("left", left)
+                .queryParam("right", right);
+        return uriComponentsBuilder.toUriString();
     }
 
     @Override
