daac4a0dc10abdd97251bb7deca81923f48b83ff
dasha.garanina <dasha.garanina@gmail.com>
2018-08-16 01:21:49 +0300

    cleanup

--- master-service/src/main/java/com/provectus/task2/controller/CalculateController.java
+++ master-service/src/main/java/com/provectus/task2/controller/CalculateController.java
@@ -5,13 +5,11 @@ import com.provectus.task2.service.Solution;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-@Slf4j
 @RestController
 @RequiredArgsConstructor
 @Api(description = "Pi calculation interface")
--- master-service/src/main/java/com/provectus/task2/controller/StatisticController.java
+++ master-service/src/main/java/com/provectus/task2/controller/StatisticController.java
@@ -5,12 +5,13 @@ import com.provectus.task2.service.StatisticService;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
-@Slf4j
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/statistic")
